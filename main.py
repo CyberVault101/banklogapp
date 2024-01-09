225739f69a101783795682be3e7429db3b9b99d2
CyberVault101 <155952072+CyberVault101@users.noreply.github.com>
2024-01-09 23:43:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-thtisc is dji'  cj
+import logging
+
+# Configure the logging
+logging.basicConfig(filename='my_log_file.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# Example usage
+logging.info('This is an informational message.')
+logging.warning('This is a warning message.')
+logging.error('This is an error message.')
